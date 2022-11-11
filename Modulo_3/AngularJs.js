28a75fcbc9b9e91edcdc5999badc31db2db40408
NaniLash <83770855+NaniLash@users.noreply.github.com>
2022-11-10 19:39:55 -0600

    Revisión de estancias ll

--- Modulo_3/AngularJs.js
+++ Modulo_3/AngularJs.js
@@ -1,4 +1,8 @@
 angular.module("voting", [])
     .controller("ctr_module_3", function($scope) {
-
+        $scope.data_table = [
+            { number: 1, title: "Desayuno", description: "¿Qué tipo de desayuno?", link: "", open: true, show: true, save: false, votes: 10 },
+            { number: 2, title: "Prueba", description: "¿Esto es una prueba?", link: "", open: true, show: false, save: false, votes: 101 },
+            { number: 3, title: "Demo", description: "Do you like it?", link: "", open: true, show: false, save: true, votes: 421 }
+        ];
     });
