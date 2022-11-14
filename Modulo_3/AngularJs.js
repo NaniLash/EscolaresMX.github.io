605c3ae2744358c40176bed4a07c342818ba0149
NaniLash <83770855+NaniLash@users.noreply.github.com>
2022-11-14 01:29:12 -0600

    Delete Modulo_3 directory

--- Modulo_3/AngularJs.js
+++
@@ -1,8 +0,0 @@
-angular.module("voting", [])
-    .controller("ctr_module_3", function($scope) {
-        $scope.data_table = [
-            { number: 1, title: "Desayuno", description: "¿Qué tipo de desayuno?", link: "", open: true, show: true, save: false, votes: 10 },
-            { number: 2, title: "Prueba", description: "¿Esto es una prueba?", link: "", open: true, show: false, save: false, votes: 101 },
-            { number: 3, title: "Demo", description: "Do you like it?", link: "", open: true, show: false, save: true, votes: 421 }
-        ];
-    });
