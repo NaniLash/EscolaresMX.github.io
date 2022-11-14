e7c1181c5de8ee097d40d24a1dc726b770f2f524
NaniLash <83770855+NaniLash@users.noreply.github.com>
2022-11-14 01:29:23 -0600

    Delete Modulo_4 directory

--- Modulo_4/AngularJs.js
+++
@@ -1,7 +0,0 @@
-angular.module("work", [])
-    .controller("ctr_module_4", function($scope) {
-        $scope.data_person = [
-            { name: "David Victor", lastname: "Berumen Ramirez", career: "Desarrollador de software", dateini: "2022-08-08", dateend: "2022-10-08", phone: "", status: "Pendiente" },
-            { name: "Francisco", lastname: "Alvarez Bueno", career: "Desarrollador de software", dateini: "2020-08-07", dateend: "2023-11-18", phone: "6771064052", status: "Pendiente" }
-        ]
-    });
